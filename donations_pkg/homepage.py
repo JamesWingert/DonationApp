2fa79ba33bd44125040677fbde6b5a01e9e70e98
Jimmy Wingert <jameswingert1@gmail.com>
2021-08-28 13:24:19 -0400

    Initial Commit

---
+++ donations_pkg/homepage.py
@@ -0,0 +1,6 @@
+def show_homepage():
+    print("""             ===DonateMe Homepage===
+          1.  Login     |   2. Register
+          3.  Donate    |   4. Show Donations
+                    5. Exit     
+          """)

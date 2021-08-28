dc4c3999f0b10511410cc53261427191aafb8acd
Jimmy Wingert <jameswingert1@gmail.com>
2021-08-28 14:00:05 -0400

    Login function completed

--- donations_pkg/user.py
+++ donations_pkg/user.py
@@ -2,3 +2,15 @@ def login(database, username, password):
     database = {}
     username = ""
     password = ""
+    
+    username, password = username.strip(), password.strip()
+    
+    if database.get(username) == password:
+       print(f"Welcome {username}")
+       return username
+    elif username in database and password not in database:
+        print("Incorrect password, please try again")
+        return ""
+    else:
+        print("Incorrect username and password")   
+        return "" 
